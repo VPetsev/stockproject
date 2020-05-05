c5a153d4a955fb4af17b8f31c7dccc34affb9075
JGarcia1998 <jared_garcia@aol.com>
2020-05-04 22:07:16 -0500

    updated main files

--- js/main.js
+++ js/main.js
@@ -2,33 +2,53 @@ let usrData = document.getElementById("usrData")
 let searchButton = document.getElementById("searchButton")
 
 let stockData = document.getElementById("stockData")
+let mainStocks = document.getElementById("stocksMain")
 
-searchButton.addEventListener("click", function() {
-fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${usrData.value}&interval=5min&outputsize=full&apikey=LTTSRB12RXT9ZBDH`)
+searchButton.addEventListener("click", function () {
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${usrData.value}&interval=5min&outputsize=full&apikey=LTTSRB12RXT9ZBDH`)
 
-.then(response => response.json())
-.then(x => { 
+        .then(response => response.json())
+        .then(x => {
 
-    metaData = x['Meta Data']
-    dailyValues = x['Time Series (5min)']    
-    keyToValue2 = Object.values(dailyValues)
-    info2 = keyToValue2[0]
+            metaData = x['Meta Data']
+            dailyValues = x['Time Series (5min)']
+            keyToValue2 = Object.values(dailyValues)
+            info2 = keyToValue2[0]
 
 
 
-    stockData.innerHTML = `<li><b>${metaData['2. Symbol'].toUpperCase()}</b> (5min Updates)</li>
+            stockData.innerHTML = `<li><button class="buttons">+</button><b> ${metaData['2. Symbol'].toUpperCase()}</b> (5min Updates)</li>
                             <p>Opening Price: ${info2['1. open']}</p>
                             <p>High: ${info2['2. high']}</p>
                             <p>Low: ${info2['3. low']}</p>
                             <p>Closing: ${info2['4. close']}</p>
-                            <p>Volume: ${info2['5. volume']}</p>`
+                            <p>Volume: ${info2['5. volume']}</p>
+                            `
 
 
-    })
-
-    
-
+        })
 
 })
 
+let stocks = ['AAPL', 'FB', 'NFLX', 'JPM', 'TWTR']
 
+for (let i = 0; i < stocks.length; i++) {
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stocks[i]}&interval=5min&outputsize=full&apikey=LTTSRB12RXT9ZBDH`)
+        .then(responseMain => responseMain.json())
+        .then(allStocks => {
+            metaData = allStocks['Meta Data']
+            dailyValues = allStocks['Time Series (5min)']
+            keyToValue2 = Object.values(dailyValues)
+            info2 = keyToValue2[0]
+
+            mainStocks.innerHTML += `<li><button class="buttons">+</button><b> ${metaData['2. Symbol'].toUpperCase()}</b> (5min Updates)</li>
+    <p>Opening Price: ${info2['1. open']}</p>
+    <p>High: ${info2['2. high']}</p>
+    <p>Low: ${info2['3. low']}</p>
+    <p>Closing: ${info2['4. close']}</p>
+    <p>Volume: ${info2['5. volume']}</p>
+    `
+        })
+
+
+}
