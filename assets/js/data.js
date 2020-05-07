247c5b9d62c161cabfc7776dd9a1cd40ca3f60b9
Victor Petsev <Vpetsev96@gmail.com>
2020-05-07 12:13:11 -0500

    extended chart range to 3months, added example news articles

--- assets/js/data.js
+++ assets/js/data.js
@@ -12,31 +12,52 @@ let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius
 function newsFunction(newsData, symbol) {
     console.log(newsData)
     let tempText = ""
+    let exampleNews = document.getElementById("example-news")
     for (let i = 0; i < newsData.length; i++) {
         let news = newsData[i]
         let sum = news.summary.substring(0, 100)
         let textcontainer =
-            `<h2>\
-            <b>Relevant news for ${symbol}</b>\
-        </h2>\
-            <p>\
-                <div class="newsContainer">\
-                    <a href="${news.url}"<img src="${news.image}></img></a>   \
-                    <div class="news-title">\
-                    <h3>${news.headline}</h3>\
-                        <h6>${news.datetime}</h6>\
-                    </div>\
-                    <span>${sum}...</span>\
-                    <a href="${news.url}"><span>source: ${news.source}: Read More <i class="dx-icon-link"></i></a></span>\
-                </div>\
-            </p>\
-            \
             `
+            <p>
+                <div class="newsContainer">
+                    <div class="news-title">
+                        <a href="${news.url}"><img src="${news.image}"></img></a>
+                        <h3>${news.headline}</h3>
+                        <h6>${news.datetime}</h6>
+                    </div>
+                    <span>${sum}...</span>
+                    <a href="${news.url}">
+                    <span>source: ${news.source} </span>
+                    <span>Read More <i class="dx-icon-link"></i></a></span>
+                </div>
+            </p>`
         tempText += textcontainer
     }
+    exampleNews.insertAdjacentHTML("beforebegin", `
+        <h2>
+            <b>Relevant news for ${symbol}</b>
+        </h2>`
+    )
+    exampleNews.innerHTML += tempText
     console.log('fired')
     text += tempText
     return tempText
 }
 
-console.log(text)
+let exampleNews = document.getElementById("example-news")
+
+exampleNews.innerHTML =
+    `<h2>
+    <b>Relevant news for ${mySymbol}</b>
+</h2>
+<p>
+    <div class="newsContainer">
+        <div class="news-title">
+            <a href="${news.url}"><img src="${news.image}"></img></a>
+            <h3>${news.headline}</h3>
+            <h6>${news.datetime}</h6>
+        </div>
+        <span>${sum}...</span>
+        <a href="${news.url}"><span>source: ${news.source}: Read More <i class="dx-icon-link"></i></a></span>
+    </div>
+</p>`
