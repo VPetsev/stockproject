7746161286074514ef52d1a9cca34a2ee3fed694
Victor Petsev <Vpetsev96@gmail.com>
2020-05-07 23:42:43 -0500

    fixed cards on main.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let txtStockw = document.getElementById("txtStock")
-let searchButtonw = document.getElementById("searchButton")
+let searchButtonw = document.getElementById("searchButtonw")
 let stockData = document.getElementById("stockData")
 let mainStocks = document.getElementById("stocksMain")
 let cardStocks = document.getElementById("cardStocks")
@@ -12,61 +12,41 @@ let watchlist = rootRef.child("Watchlist")
 searchButtonw.addEventListener("click", function () {
     fetch(`https://sandbox.iexapis.com/stable/stock/${txtStockw.value}/chart/dynamic?token=Tsk_f505cc8d1a8e429e9f06fc365bb67dbb`)
         .then(response => response.json())
-        .then(x => {
-            
-            txtStock.value = ""
-            metaData = x['Meta Data']
-            dailyValues = x['Time Series (5min)']
-            keyToValue2 = Object.values(dailyValues)
-            info2 = keyToValue2[0]
+        .then(allInfo => {
+            // Dynamic call - How to handle different formats for after hours vs trading hours?
+            console.log(allInfo)
+            let symbol = txtStockw.value
+            console.log(symbol)
+            function stockPercentageUp(symbol) {
+                let resultw = (symbol.close - symbol.open)
+                let result2w = parseFloat(resultw).toFixed(2)
+                return result2w
+            }
 
-
-            cardStocks.innerHTML = `<div class="card" style="width: 18rem; display: inline-block;">
+            cardStocks.innerHTML = 
+            `<div class="card" style="width: 18rem; display: inline-block;">
             <img src="https://c1.wallpaperflare.com/preview/297/171/764/chart-trading-courses-analysis.jpg" class="card-img-top" alt="logo" styles=""    border-radius: "25px 10px 0px 0px">
             <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-             
-                </h5>
                 <p class="card-text">
                     <ul id='cardText' style="text-align: left;">
                     <button class="buttons" onclick="addToWatchlist('${symbol}')">+</button>
-                    <li><b> ${metaData['2. Symbol'].toUpperCase()}</b> (Today's Latest Data) <p id="percentage" style="${stockPercentageUp() > 0 ? 'color:rgb(88, 212, 88' : 'color:red'}">${stockPercentageUp()}</p></li>
-                            <p>Opening Price: ${info2['1. open']}</p>
-                            <p>High: ${info2['2. high']}</p>
-                            <p>Low: ${info2['3. low']}</p>
-                            <p>Closing: ${info2['4. close']}</p>
-                            <p>Volume: ${info2['5. volume']}</p>
-                        
+                    <li><b> ${symbol.toUpperCase()}</b> (Today's Latest Data) <p id="percentage" style="${stockPercentageUp(symbol) > 0 ? 'color:rgb(88, 212, 88' : 'color:red'}">${stockPercentageUp(symbol)}</p></li>
+                            <p>Opening Price: ${allInfo.open}</p>
+                            <p>High: ${allInfo.high}</p>
+                            <p>Low: ${allInfo.low}</p>
+                            <p>Closing: ${allInfo.close}</p>
+                            <p>Volume: ${allInfo.volume}</p>
                     </ul>
-                <a href="stock-info.html" class="btn btn-primary">See more about this stock</a>
+                <a href="stock-info.html" onclick="grabInfo'${symbol}'"class="btn btn-primary">See more about this stock</a>
             </div>
         </div>`
+        
            
-
-
-            cardStocks.innerHTML += `<div class="card" style="width: 18rem; display: inline-block;">
-                                            <img src="https://c1.wallpaperflare.com/preview/297/171/764/chart-trading-courses-analysis.jpg" class="card-img-top" alt="logo" border-radius: "25px 10px 0px 0px">
-                                            <div class="card text-white bg-dark mb-3" style="margin-bottom: 0px!important; max-width: 18rem;">
-                                                <p class="card-text">
-                                                    <ul id='cardText' style="text-align: left;">
-                                                    <button class="buttons" onclick="addToWatchlist('${stocks[i]}')">+</button> 
-                                                    <li><b> ${stocks[i]}</b> (Today's Latest Data)<p id="percentage" style="${allInfo.change > 0 ? 'color:rgb(88, 212, 88' : 'color:red'}">${allInfo.change > 0 ? `+${parseFloat(allInfo.change).toFixed(2)}(${allInfo.changePercent}%)` : `${parseFloat(allInfo.change).toFixed(2)}(${allInfo.changePercent}%)`}</p></li>
-                                                    
-                                                <p>Today's High: ${allInfo.high}</p>
-                                                <p>Today's Low: ${allInfo.low}</p>
-                                                <p>Recent Closing: ${allInfo.close}</p>
-                                                <p>Opening Price: ${allInfo.open}</p>
-                                                <p>Volume: ${allInfo.volume} Shares</p>   
-                                                <p>Date: ${allInfo.date}</p>
-                                                </ul>
-            
-                                                <a href="stock-info.html"><button onclick="grabInfo('${allInfo.symbol}')" class="btn btn-primary">See more about this stock</button></a>
-                                            </div>
-                                        </div>`
-        })
+})
 })
 
 
-//  STOCKS RAN ON HOME PAGE AUTOMATICALLY 
+//  STOCKS RAN ON WATCHLIST PAGE AUTOMATICALLY 
 let stocks = ['AAPL', 'FB', 'NFLX']
 
 for (let i = 0; i < stocks.length; i++) {
@@ -74,40 +54,43 @@ for (let i = 0; i < stocks.length; i++) {
         .then(response => response.json())
         .then(allInfo => {
             console.log(allInfo)
+            let symbol = stocks[i]
+
+            function stockPercentageUp(symbol) {
+                let resultw = (symbol.close - symbol.open)
+                let result2w = parseFloat(resultw).toFixed(2)
+                return result2w
+            }
 
             cardStocks.innerHTML += `<div class="card" style="width: 18rem; display: inline-block;">
-                                            <img src="https://c1.wallpaperflare.com/preview/297/171/764/chart-trading-courses-analysis.jpg" class="card-img-top" alt="logo" border-radius: "25px 10px 0px 0px">
-                                            <div class="card text-white bg-dark mb-3" style="margin-bottom: 0px!important; max-width: 18rem;">
-                                                <p class="card-text">
-                                                    <ul id='cardText' style="text-align: left;">
-                                                    <button class="buttons" onclick="addToWatchlist('${symbol}'); window.alert('${symbol} Successfully Added to Watchlist!')" >+</button> 
-                                                    <li><b> ${symbol}</b> (Today's Latest Data)<p id="percentage" style="${stockPercentageUp() > 0 ? 'color:rgb(88, 212, 88' : 'color:red'}">${stockPercentageUp()}</p></li>
-                                                    
-                                                <p>Today's High: ${allInfo.high}</p>
-                                                <p>Today's Low: ${allInfo.low}</p>
-                                                <p>Recent Closing: ${allInfo.close}</p>
-                                                <p>Opening Price: ${allInfo.open}</p>
-                                                <p>Volume: ${allInfo.volume} Shares</p>  
-                                                <p>Date: ${allInfo.date}</p> 
-                                                </ul>
-            
-                                            <a href="stock-info.html" style="text-align: center;><button onclick="grabInfo('${allInfo.symbol}')" class="btn btn-primary">See more about this stock</button></a>
-                                            </div>
-                                        </div>`
+                <img src="https://c1.wallpaperflare.com/preview/297/171/764/chart-trading-courses-analysis.jpg" class="card-img-top" alt="logo" border-radius: "25px 10px 0px 0px">
+                <div class="card text-white bg-dark mb-3" style="margin-bottom: 0px!important; max-width: 18rem;">
+                    <p class="card-text">
+                        <ul id='cardText' style="text-align: left;">
+                        <button class="buttons" onclick="addToWatchlist('${symbol}'); window.alert('${symbol} Successfully Added to Watchlist!')" >+</button> 
+                        <li><b> ${symbol}</b> (Today's Latest Data)<p id="percentage" style="${stockPercentageUp(symbol) > 0 ? 'color:rgb(88, 212, 88' : 'color:red'}">${stockPercentageUp(symbol)}</p></li>
+                        
+                    <p>Today's High: ${allInfo.high}</p>
+                    <p>Today's Low: ${allInfo.low}</p>
+                    <p>Recent Closing: ${allInfo.close}</p>
+                    <p>Opening Price: ${allInfo.open}</p>
+                    <p>Volume: ${allInfo.volume} Shares</p>  
+                    <p>Date: ${allInfo.date}</p> 
+                    </ul>
+
+                <a href="stock-info.html" style="text-align: center;><button onclick="grabInfo('${symbol}')" class="btn btn-primary">See more about this stock</button></a>
+                </div>
+            </div>`
         })
+
 }
 
 function addToWatchlist(symbol) {
-
     watchlist.push({
         stock: symbol
     })
     watchlist.on("value", function(snapshot){
         array = Object.values(snapshot.val())
-        
-        
-
-        
     })  
 
 }
