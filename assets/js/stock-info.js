6f7402fbab84b04b0fee8a345f25df1a3e15a36f
Victor Petsev <Vpetsev96@gmail.com>
2020-05-07 19:25:48 -0500

    removed dx drawer widget, added a bootstrap container for news elements, seperated api functions into historical data, news, and company information

--- assets/js/stock-info.js
+++ assets/js/stock-info.js
@@ -1,16 +1,16 @@
 let mySymbol = sessionStorage.mySymbol
-parseData(mySymbol, 'chart', testFunction)
+parseData(mySymbol, chartFunction)
 
-function parseData(stock, type, callback) {
-    fetch(`https://sandbox.iexapis.com/stable/stock/${stock}/batch?types=news,chart&range=3m&last=5&token=Tsk_f505cc8d1a8e429e9f06fc365bb67dbb`)
+function parseData(stock, callback) {
+    fetch(`https://sandbox.iexapis.com/stable/stock/${stock}/chart/3m?token=Tsk_f505cc8d1a8e429e9f06fc365bb67dbb`)
         .then(responseMain => responseMain.json())
         .then(allInfo => {
-            const typeOfData = allInfo[`${type}`]
-            callback(typeOfData, stock)
+            callback(allInfo, stock)
         })
 }
 
-function testFunction(data, symbol) {
+
+function chartFunction(data, symbol) {
     console.log(data, symbol)
     $(function () {
         var chart = $("#zoomedChart").dxChart({
@@ -94,85 +94,46 @@ function testFunction(data, symbol) {
     });
 }
 
-function companyInfo(stock, callback) {
-    fetch(`https://sandbox.iexapis.com/stable/stock/${stock}/company?token=Tsk_f505cc8d1a8e429e9f06fc365bb67dbb`)
+function companyInfo(stock) {
+    fetch(`https://sandbox.iexapis.com/stable/stock/${stock}/company?token=Tpk_4aa9dbaa5e0d48d497c96a35ce0d7493`)
         .then(responseMain => responseMain.json())
         .then(allInfo => {
+            let companyInfo = document.getElementById("company-information")
             console.log(allInfo)
-            text = `
+            companyInfo.innerHTML = `
                     <h2><b>${allInfo.companyName}</b><h3>(${allInfo.symbol})</h3></h2>
                     <span>
                         <p>Company Summary: ${allInfo.description}</p>
                         <span>Website: ${allInfo.website}</span>
                         <p>Address: ${allInfo.address} ${allInfo.city}, ${allInfo.state}, ${allInfo.country}</p>
                     </span>`
-            callback(allInfo, text)
         })
 }
+companyInfo(mySymbol)
 
-companyInfo(mySymbol, drawerFunction)
-
-function drawerFunction(allInfo, text) {
-    console.log(allInfo, text)
-    $(function () {
-        $("#content").html(text);
-
-        var drawer = $("#drawer").dxDrawer({
-            opened: false,
-            height: 400,
-            closeOnOutsideClick: true,
-            template: function () {
-                var $list = $("<div>").width(200).addClass("panel-list");
-
-                return $list.dxList({
-                    dataSource: navigation,
-                    hoverStateEnabled: false,
-                    focusStateEnabled: false,
-                    activeStateEnabled: false,
-                    elementAttr: { class: "dx-theme-accent-as-background-color" }
-                });
-            }
-        }).dxDrawer("instance");
-
-        $("#toolbar").dxToolbar({
-            items: [{
-                widget: "dxButton",
-                location: "before",
-                options: {
-                    icon: "menu",
-                    onClick: function () {
-                        drawer.toggle();
-                    }
-                }
-            }]
-        });
-
-        $("#reveal-mode").dxRadioGroup({
-            items: ["slide", "expand"],
-            layout: "horizontal",
-            value: "slide",
-            onValueChanged: function (e) {
-                drawer.option("revealMode", e.value);
-            }
-        });
-
-        $("#opened-state-mode").dxRadioGroup({
-            items: ["push", "shrink", "overlap"],
-            layout: "horizontal",
-            value: "shrink",
-            onValueChanged: function (e) {
-                drawer.option("openedStateMode", e.value);
-                $("#reveal-mode-option").css("visibility", e.value !== "push" ? "visible" : "hidden");
-            }
-        });
-
-        $("#position-mode").dxRadioGroup({
-            items: ["left", "right"],
-            layout: "horizontal",
-            value: "left",
-            onValueChanged: function (e) {
-                drawer.option("position", e.value);
-            }
-        });
-    });
+function newsFunction(symbol) {
+    fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/batch?types=news&last=5&token=Tpk_4aa9dbaa5e0d48d497c96a35ce0d7493`)
+    .then(response => response.json())
+    .then(allInfo => {
+        let exampleNews = document.getElementById("example-news")
+        allInfo.forEach(element => {
+            exampleNews.innerHTML = `
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="${element.image}" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                            <a href="${element.url}>
+                                <h5 class="card-title">${element.headline}</h5>
+                                <p class="card-text">${element.summary}</p>
+                                <p class="card-text"><small class="text-muted">Published at: ${element.datetime} | From: ${element.source}</small></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+        })
+    })
 }
