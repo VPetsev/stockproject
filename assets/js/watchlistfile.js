9ad90724c1055b883bc9d5af00f1cd449bc402f2
Victor Petsev <Vpetsev96@gmail.com>
2020-05-08 01:40:17 -0500

    moved main.js into watchlist.js, adding functionality to stock-info.js

--- assets/js/watchlistfile.js
+++ assets/js/watchlistfile.js
@@ -1,12 +1,14 @@
 let txtStockw = document.getElementById("txtStockw")
 let cardStocksw = document.getElementById("cardStocksw")
 let stockDataw = document.getElementById("stockDataw")
+let searchButtonw = document.getElementById("searchButtonw")
+let mainStocks = document.getElementById("stocksMain")
 
 // FIREBASE
 let databasex = firebase.database()
 let rootRefx = databasex.ref()
 let authx = firebase.auth()
-let watchlist2 = rootRef.child("Watchlist")
+let watchlist2 = rootRefx.child("Watchlist")
 
 // SEARCH FUNCTION
 
@@ -98,19 +100,69 @@ function x(array) {
     }
 }
 
+
+function addToWatchlist(symbol) {
+    watchlist.push({
+        stock: symbol
+    })
+    watchlist.on("value", function (snapshot) {
+        array = Object.values(snapshot.val())
+    })
+
+}
+
 // DELETE FUNCTION AND CLEARING WATCHLIST
 
 function deleteStockx() {
     rootRefx.child("Watchlist").remove()
     console.log("fired")
 }
+// NOTE: Not added yet
+// let clearWatchlist = document.getElementById("clearWatchlist")
 
-let clearWatchlist = document.getElementById("clearWatchlist")
+// clearWatchlist.addEventListener("click", function () {
+//     deleteStockx()
+//     cardStocksw.innerHTML = ""
+//     window.alert("Watchlist Sucessfully Cleared!")
+// })
 
-clearWatchlist.addEventListener("click", function () {
-    deleteStockx()
-    cardStocksw.innerHTML = ""
-    window.alert("Watchlist Sucessfully Cleared!")
-})
+
+//  STOCKS RAN ON WATCHLIST PAGE ONLOAD
+let stocks = ['AAPL', 'FB', 'NFLX']
+
+for (let i = 0; i < stocks.length; i++) {
+    fetch(`https://sandbox.iexapis.com/stable/stock/${stocks[i]}/chart/dynamic?token=Tsk_f505cc8d1a8e429e9f06fc365bb67dbb`)
+        .then(response => response.json())
+        .then(allInfo => {
+            let symbol = stocks[i]
+            let mostRecent = allInfo.data.slice(-1).pop()
+
+            function stockPercentageUp(symbol) {
+                let resultw = (symbol.close - symbol.open)
+                let result2w = parseFloat(resultw).toFixed(2)
+                return result2w
+            }
+
+            cardStocksw.innerHTML += `<div class="card" style="width: 18rem; display: inline-block;">
+                <img src="https://c1.wallpaperflare.com/preview/297/171/764/chart-trading-courses-analysis.jpg" class="card-img-top" alt="logo" border-radius: "25px 10px 0px 0px">
+                <div class="card text-white bg-dark mb-3" style="margin-bottom: 0px!important; max-width: 18rem;">
+                    <p class="card-text">
+                        <ul id='cardText' style="text-align: left;">
+                        <button class="buttons" onclick="addToWatchlist('${symbol}')" window.alert('${symbol} Successfully Added to Watchlist!')" >+</button> 
+                        <li><b> ${symbol}</b> (Today's Latest Data)<p id="percentage" style="${stockPercentageUp(mostRecent) > 0 ? 'color:rgb(88, 212, 88' : 'color:red'}">${stockPercentageUp(mostRecent)}</p></li>
+                        
+                    <p>Today's High: ${mostRecent.high}</p>
+                    <p>Today's Low: ${mostRecent.low}</p>
+                    <p>Recent Closing: ${mostRecent.close}</p>
+                    <p>Opening Price: ${mostRecent.open}</p>
+                    <p>Volume: ${mostRecent.volume} Shares</p>  
+                    <p>Date: ${mostRecent.date}</p> 
+                    </ul>
+
+                <a href="stock-info.html" style="text-align: center;><button onclick="grabInfo('${symbol}')" class="btn btn-primary">See more about this stock</button></a>
+                </div>
+            </div>`
+        })
+}
 
 
